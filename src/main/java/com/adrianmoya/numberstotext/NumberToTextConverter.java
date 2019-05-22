16dd764e2ad48475c0ee6fe19c28e3f0b7d254f3
Adrian Moya <adrianmoya@gmail.com>
2019-05-22 15:44:32 -0500

    Handling thousands

--- src/main/java/com/adrianmoya/numberstotext/NumberToTextConverter.java
+++ src/main/java/com/adrianmoya/numberstotext/NumberToTextConverter.java
@@ -11,45 +11,60 @@ public class NumberToTextConverter {
 
         int intNumber = Integer.valueOf(number);
 
-        if(intNumber < 10){
-            return NumberDictionary.ONES_DICTIONARY.get(number);
+        if (intNumber < 10) {
+            return convertOnes(number);
         }
 
-        if(intNumber >= 10 && intNumber <99){
+        if (intNumber >= 10 && intNumber < 100) {
             return convertTens(number);
         }
 
-        if(intNumber >= 100 && intNumber <1000){
-            // Get position in the HUNDREDS_DICTIONARY
-            String[] digits = number.split("(?<=.)");
-            sb.append(NumberDictionary.ONES_DICTIONARY.get(digits[0]));
-            sb.append(" HUNDRED ");
-            sb.append(convertNumber(digits[1] + digits[2]));
+        if (intNumber >= 100 && intNumber < 1000) {
+            return convertHundreds(number);
+        }
+
+        if (intNumber >= 1000 && intNumber < 1000000) {
+            String thousandChunk = number.substring(0, number.length() - 3);
+            sb.append(convertNumber(thousandChunk)).append(" THOUSAND ");
+            sb.append(convertNumber(number.substring(number.length() - 3, number.length())));
             return sb.toString();
         }
         throw new Exception("Can't convert this number yet");
     }
 
+    private static String convertHundreds(String number) throws Exception {
+        StringBuilder sb = new StringBuilder();
+        // Get position in the HUNDREDS_DICTIONARY
+        String[] digits = number.split("(?<=.)");
+        sb.append(NumberDictionary.ONES_DICTIONARY.get(digits[0]));
+        sb.append(" HUNDRED ");
+        sb.append(convertNumber(digits[1] + digits[2]));
+        return sb.toString();
+    }
 
-    private static String convertTens(String number){
+    private static String convertTens(String number) {
         int intNumber = Integer.valueOf(number);
         StringBuilder sb = new StringBuilder();
 
-        if(intNumber >=10 && intNumber < 20) {
+        if (intNumber >= 10 && intNumber < 20) {
             // Get position in the TENS_DICTIONARY
             return NumberDictionary.TENS_10_TO_19_DICTIONARY.get(number);
         }
 
-        if(intNumber >= 20 && intNumber < 100) {
+        if (intNumber >= 20 && intNumber < 100) {
             // Get position in the TENS_20_TO_90_DICTIONARY
             String[] digits = number.split("(?<=.)");
             sb.append(NumberDictionary.TENS_20_TO_90_DICTIONARY.get(digits[0]));
-            if(!"0".equals(digits[1])) {
+            if (!"0".equals(digits[1])) {
                 sb.append(" ");
-                sb.append(NumberDictionary.ONES_DICTIONARY.get(digits[1]));
+                sb.append(convertOnes(digits[1]));
             }
             return sb.toString();
         }
         return "";
+    }
+
+    private static String convertOnes(String digit) {
+        return NumberDictionary.ONES_DICTIONARY.get(digit);
     }
 }
--- src/test/java/com/adrianmoya/numberstotext/NumberToTextConverterTest.java
+++ src/test/java/com/adrianmoya/numberstotext/NumberToTextConverterTest.java
@@ -69,4 +69,30 @@ public class NumberToTextConverterTest {
         assertThat("Result was not TWO HUNDRED TWENTY SEVEN", result, equalTo("TWO HUNDRED TWENTY SEVEN"));
     }
 
+    @Test
+    public void shouldConvertNumbersInTheThousandsPlace() throws Exception {
+        // Arrange
+        String number = "34227";
+
+        // Act
+        String result = NumberToTextConverter.convertNumber(number);
+
+        // Assert
+        assertThat("Result was not THIRTY FOUR THOUSAND TWO HUNDRED TWENTY SEVEN", result,
+                equalTo("THIRTY FOUR THOUSAND TWO HUNDRED TWENTY SEVEN"));
+    }
+
+    @Test
+    public void shouldConvertNumbersInTheThousandsPlaceBorder() throws Exception {
+        // Arrange
+        String number = "999999";
+
+        // Act
+        String result = NumberToTextConverter.convertNumber(number);
+
+        // Assert
+        assertThat("Result was not NINE HUNDRED NINETY NINE THOUSAND NINE HUNDRED NINETY NINE", result,
+                equalTo("NINE HUNDRED NINETY NINE THOUSAND NINE HUNDRED NINETY NINE"));
+    }
+
 }
