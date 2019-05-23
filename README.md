474b321eca4d047d1b8050df8bb5f9e57549f8bd
Adrian Moya <adrianmoya@gmail.com>
2019-05-22 20:59:25 -0500

    More info to readme

--- README.md
+++ README.md
@@ -4,11 +4,14 @@ In order to run this excersize, follow these steps:
 
 1. Clone the repository
 
+`git clone https://github.com/adrianmoya/numbers-to-text.git`
+
 2. Compile and package with the command:
 
 `./mvnw clean package`
 
 3. Run the app with the command:
 
-`java -jar target/numbers-to-text-1.0.jar`
+`java -jar target/numbers-to-text-1.0.jar <number>`
 
+where <number> is the number you want to convert to text.
--- src/main/java/com/adrianmoya/numberstotext/App.java
+++ src/main/java/com/adrianmoya/numberstotext/App.java
@@ -6,20 +6,26 @@ package com.adrianmoya.numberstotext;
  */
 public class App {
     public static void main(String[] args) {
+
+        if(args.length < 1 || args.length > 1) {
+            printUsage();
+            System.exit(1);
+        }
         String input = args[0];
         try {
             // Save check to see if it's a correctly formatted number within possible range
             Long.valueOf(input);
-            
             String result = NumberToTextConverter.convertNumber(input);
             System.out.println(result);
         } catch (NumberFormatException e) {
-            System.out.println("Invalid input, please type a valid number. Examples: 23456, -345");
-            System.out.println(e.getMessage());
+            printUsage();
         } catch (Exception e) {
             System.out.println("Cannot convert that number");
-            System.out.println(e.getMessage());
         }
 
+    }
+
+    private static void printUsage() {
+        System.out.println("Invalid input, please type a valid number. Examples: 23456, -345");
     }
 }
